fe6a55df9fc8fb4bb1217c57813eba6a82021237
DESKTOP-7KGV365\ASUS <saridinipuspita@gmail.com>
2018-06-12 21:15:56 +0700

    controllers login

---
+++ application/controllers/Login.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit ('No direct script access allowed');
+class Login extends CI_Controller {
+	
+	public function index()
+	{
+		$this->load->view('LoginView');
+	}
+	public function cekLogin()
+	{
+		$this->load->Library('form_validation');
+		$this->form_validation->set_rules('username', 'Username', 'trim|required');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|callback_cekDb');
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('LoginView');
+		}else{
+			redirect('Login1/home','refresh');
+		}
+	}
+	public function cekDb($password)
+	{
+		$this->load->model('user');
+		$username = $this->input->post('username');
+		$result = $this->user->login($username,$password);
+		if($result){
+			$sess_aaray = array();
+			foreach ($result as $row) {
+				$sess_array = array('id_user'=>$row->id_user,'username'=> $row->username);
+				$this->session->set_userdata('logged_in',$sess_array);
+			}
+			return true;
+		}else{
+			$this->form_validation->set_message('cekDb',"Login Gagal Username dan Password tidak valid");
+			return false;
+			}
+	}
+	public function Logout()
+	{
+		$this->session->unset_userdata('logged_in');
+		$this->session->sess_destroy();
+		redirect('login','refresh');
+		}
+}
