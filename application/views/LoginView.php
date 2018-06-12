95a7cf7a6c0c7d4dcf2fa927a5ec4b20cfdedae5
DESKTOP-DF4BIOU\rioir <rioirvansyah6@gmail.com>
2018-06-12 22:11:39 +0700

    Konfigurasi config + autoload

--- application/views/LoginView.php
+++ application/views/LoginView.php
@@ -21,10 +21,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<body>
 		<h1 class="text-center">Silahkan Login</h1>
 
-		<!-- jQuery -->
-		<script src="//code.jquery.com/jquery.js"></script>
-		<!-- Bootstrap JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		
 		
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
  		<div class="container">
@@ -57,5 +54,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
  	<?php echo form_close(); ?>
 
-	
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </html>
